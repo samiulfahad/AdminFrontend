cf3d973504a8ac3fc3f08f9985602aa4c25a5704
samiulfahad <samiul_fahad@yahoo.com>
2026-03-21 00:57:51 +0600

    First commit

--- src/api/baseAPI.js
+++ src/api/baseAPI.js
@@ -1,11 +1,11 @@
 // src/services/api.js
 import axios from "axios";
 
-const cloud = "https://labpilotclient-backend.onrender.com/api/v1";
+const cloud = "https://adminbackend-xwfs.onrender.com/api/v1";
 const local = "http://localhost:5000/api/v1";
 
 const api = axios.create({
-  baseURL: local,
+  baseURL: cloud,
   timeout: 10000,
 });
 
