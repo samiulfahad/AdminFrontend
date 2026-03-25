04739a6ea8f1bee884bd17511a50bc8f96680016
samiulfahad <samiul_fahad@yahoo.com>
2026-03-26 07:28:32 +0800

    labId = lab._id

--- src/api/baseAPI.js
+++ src/api/baseAPI.js
@@ -5,7 +5,7 @@ const cloud = "https://adminbackend-xwfs.onrender.com/api/v1";
 const local = "http://localhost:5000/api/v1";
 
 const api = axios.create({
-  baseURL: local,
+  baseURL: cloud,
   timeout: 10000,
 });
 
