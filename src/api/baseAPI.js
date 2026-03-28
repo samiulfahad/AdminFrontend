55b37fad497067267ffb3e5e46a5533da23a04f3
samiulfahad <samiul_fahad@yahoo.com>
2026-03-28 21:49:44 +0600

    env added

--- src/api/baseAPI.js
+++ src/api/baseAPI.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const render = "https://adminbackend-xwfs.onrender.com/api/v1";
-const railway = "https://adminbackend-production-bf1f.up.railway.app/api/v1"
+const railway = "adminbackend-production-df9b.up.railway.app/api/v1"
 const local = "http://localhost:5000/api/v1";
 
 const api = axios.create({
