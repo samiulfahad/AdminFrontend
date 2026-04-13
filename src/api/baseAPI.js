40effb7f15d57b94adace8558cd772a799fad3fa
samiulfahad <samiul_fahad@yahoo.com>
2026-04-13 16:27:23 +0600

    bug fixed

--- src/api/baseAPI.js
+++ src/api/baseAPI.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const render = "https://adminbackend-xwfs.onrender.com/api/v1";
-const railway = "adminbackend-production-df9b.up.railway.app/api/v1"
+const railway = "https://adminbackend-production-df9b.up.railway.app/api/v1"
 const local = "http://127.0.0.1:5000/api/v1";
 
 const api = axios.create({
